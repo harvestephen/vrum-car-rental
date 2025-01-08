f17922e42058720db0135734479a3182a5d02d81
AndreYmasa <andreymasa62@gmail.com>
2025-01-09 00:06:08 +0800

    .

--- pages/Modal/addcar_modal.php
+++ pages/Modal/addcar_modal.php
@@ -4,23 +4,26 @@
     <div class="addCarModalBox">
         <h2 class="termsModalHeader">Add Car</h2>
         <form class="modalForm" id="addCarForm">
-            <label for="car-name">Car Name</label>
-                <input type="text" id="car-name" placeholder="Car Name" />
-            
-            <label for="carTransmission">Transmission</label>
-            <select id="carTransmission">
-                <option value="">Select Transmission</option>
-                <option value="automatic">Automatic</option>
-                <option value="manual">Manual</option>
-            </select>
+            <div>
+                <input class="carName" type="text" placeholder="Car Name" />
+            </div>
 
-            <label for="carType">Type</label>
-            <select id="carType">
-                <option value="">Select Type</option>
-                <option value="sedan">Sedan</option>
-                <option value="suv">SUV</option>
-                <option value="truck">Truck</option>
-            </select>
+            <div>
+                <select id="carTransmission">
+                    <option value="">Select Transmission</option>
+                    <option value="automatic">Automatic</option>
+                    <option value="manual">Manual</option>
+                </select>
+            </div>
+
+            <div>
+                <select id="carType">
+                    <option value="">Select Type</option>
+                    <option value="sedan">Sedan</option>
+                    <option value="suv">SUV</option>
+                    <option value="truck">Truck</option>
+                </select>
+            </div>
 
             <div class="modalFooter">
                 <button class="addBtn" onclick="addCar()">Add</button>
--- pages/Template/navBar.php
+++ pages/Template/navBar.php
@@ -108,37 +108,3 @@ $rootDirectory = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/'); // Get root direc
         </div>
     </div>
 </div>
-
-<!-- Add Car Modal -->
-<div id="addCarModal" class="modal">
-    <div class="addCarModalBox">
-        <h2 class="termsModalHeader">Add Car</h2>
-        <form class="modalForm" id="addCarForm">
-            <div>
-                <input class="carName" type="text" placeholder="Car Name" />
-            </div>
-
-            <div>
-                <select id="carTransmission">
-                    <option value="">Select Transmission</option>
-                    <option value="automatic">Automatic</option>
-                    <option value="manual">Manual</option>
-                </select>
-            </div>
-
-            <div>
-                <select id="carType">
-                    <option value="">Select Type</option>
-                    <option value="sedan">Sedan</option>
-                    <option value="suv">SUV</option>
-                    <option value="truck">Truck</option>
-                </select>
-            </div>
-
-            <div class="modalFooter">
-                <button class="addBtn" onclick="addCar()">Add</button>
-                <button class="cancelBtn" onclick="closeModal()">Cancel</button>
-            </div>
-        </form>
-    </div>
-</div>
