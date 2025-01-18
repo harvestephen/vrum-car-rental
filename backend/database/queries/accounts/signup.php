552bc151c81d2470b2cd6acf20792da22257328d
harv <harveystephen.guimalan@gmail.com>
2025-01-18 21:54:57 +0800

    Refactor signup redirection logic: update root URL handling and streamline session management for improved navigation

--- backend/database/queries/accounts/signup.php
+++ backend/database/queries/accounts/signup.php
@@ -1,21 +1,18 @@
 <?php
 $rootDirectory = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/'); // Get root directory
-$rootURL = dirname('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
+$rootURL = str_replace("/backend/database/queries/accounts", "", $rootDirectory);
 if (!empty($_POST)) {
   session_start();
   $_SESSION['role'] = $_POST['role'];
   $_SESSION['username'] = $_POST['username'];
-  echo session_status();
-  header("Location: /Vrum");
+  header("Location: " . $rootURL);
 } else {
   session_start();
   if (session_id() && !empty($_SESSION['username'])) {
-    echo $_SESSION['role'];
-    header("Location: /Vrum");
+    header("Location: " . $rootURL);
   } else {
     session_destroy();
     $_SESSION['role'] = null;
-    echo $_SESSION['role'];
-    header("Location: /Vrum");
+    header("Location: " . $rootURL);
   }
 }
